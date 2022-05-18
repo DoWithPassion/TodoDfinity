1303a0e51a433f5cf0981078abde6b8219306c54
Raviteja <venkataravitejagullapudi@gmail.com>
2022-05-18 12:36:30 +0530

    Last commit

--- webpack.config.js
+++ webpack.config.js
@@ -133,7 +133,7 @@ function generateWebpackConfigForCanister(name, info) {
         ],
         devServer: {
             proxy: {
-                '/api': 'http://localhost:8000'
+                '/api': 'http://localhost:8001'
             }
         }
     };
